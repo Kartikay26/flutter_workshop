4d9c43023c788a0920c766e8d2f5710d934fc23b
Kumar Karikay <krkartikay@gmail.com>
2019-03-13 21:54:04 +0530

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Hello World"),
